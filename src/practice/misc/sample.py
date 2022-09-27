5cfd777818ca6cad93ce770722f88ceb1c2d2d31
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-09-27 11:25:13 +0530

    Python Basic Programming

---
+++ src/practice/misc/sample.py
@@ -0,0 +1,5 @@
+n1 = int(input('Enter the Valu of N1::'))
+n2 = int(input('Enter the Valu of N2::'))
+
+print('The Sum::',n1+n2)
+print('The Product::',n1*n2)
---
+++ src/practice/python_basic_Programming/add_two_number.py
@@ -0,0 +1,16 @@
+def add_two_number(number_1,number_2):
+    ''' This add_two_number function adds two numbers'''
+    return number_1 + number_2
+
+def get_a_number(x):
+    '''Method return number enter by User'''
+    return input('Enter a {0} number::'.format(x))
+# Refactor vser input
+
+# Stored input for Future Use
+number_1 = get_a_number('1st')
+number_2= get_a_number('2nd')
+
+sum = add_two_number(number_1,number_2)
+
+print(sum)
